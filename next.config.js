cfd839ebe1110f082e2c58daf28a929457cd70ac
Sunil H N <sunilh.n27@gmail.com>
2024-03-15 00:12:00 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: 'export',
-    experimental: {
-        images: true,
-    },
+
     env: {
         USERNAME: process.env.USERNAME,
         PASSWORD: process.env.PASSWORD,
-        TO: process.env.To
+        TO: process.env.TO
 
     }
 
